801599b6d84b574db5521eacabce946863622837
Vincent Cote-Roy <vincentcr@gmail.com>
2018-06-03 09:49:27 -0400

    remove scruff

--- cloud/message-processor/src/index.ts
+++ cloud/message-processor/src/index.ts
@@ -22,18 +22,6 @@ export async function handler(data: DynamoDBStreamEvent, context: Context) {
   } catch (err) {
     console.log("failed to process event with payload", data, "\nerror:", err);
   }
-
-  // const thingID = parseThingID(payload.topic);
-  // try {
-  //   await processPayload({ thingID, payload });
-  // } catch (err) {
-  //   console.log(
-  //     "failed to process event with payload",
-  //     payload,
-  //     "\nerror:",
-  //     err,
-  //   );
-  // }
 }
 
 async function processAll(data: DynamoDBStreamEvent) {
@@ -48,20 +36,9 @@ function parseStreamData(event: DynamoDBStreamEvent): StoredEvent[] {
   return event.Records.filter(({ eventName }) => eventName === "INSERT").map(
     ({ dynamodb }) => {
       const img = dynamodb!.NewImage!;
-      const evt = <StoredEvent>AWS.DynamoDB.Converter.unmarshall(img);
+      const evt = AWS.DynamoDB.Converter.unmarshall(img) as StoredEvent;
       console.log("evt =>", evt);
       return evt;
     },
   );
 }
-
-// function* parseStreamData() {}
-
-// function parseThingID(topic: string): string {
-//   const match = /^.+\/(.+?)\/(.+?)$/.exec(topic);
-//   if (match == null) {
-//     throw new Error("unexpected topic: unable to extract thingID: " + topic);
-//   }
-
-//   return match[1];
-// }
