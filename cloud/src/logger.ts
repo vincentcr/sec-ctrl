be390df1d448cc1e8b68e34e82b0eae6ccba4a28
Vincent Cote-Roy <vincentcr@gmail.com>
2018-06-30 09:46:35 -0400

    add standard bunyan serializers

--- cloud/src/logger.ts
+++ cloud/src/logger.ts
@@ -1,6 +1,10 @@
 import * as bunyan from "bunyan";
 
-const logger = bunyan.createLogger({ name: "api", level: "debug" });
+const logger = bunyan.createLogger({
+  name: "api",
+  level: "debug",
+  serializers: bunyan.stdSerializers
+});
 export default logger;
 
 export function die(err: Error, ...args: any[]) {
--- local/src/logger.ts
+++ local/src/logger.ts
@@ -2,7 +2,11 @@ import * as path from "path";
 
 import * as bunyan from "bunyan";
 
-const logger = bunyan.createLogger({ name: "event-processor", level: "debug" });
+const logger = bunyan.createLogger({
+  name: "event-processor",
+  level: "debug",
+  serializers: bunyan.stdSerializers
+});
 
 export default function createLogger(fname: string) {
   const { name } = path.parse(fname);
