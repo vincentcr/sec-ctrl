8c3c5c53023e76099bb98ce4880af9f8324388aa
Vincent Cote-Roy <vincentcr@gmail.com>
2018-08-01 21:12:11 -0400

    local: refactor local site for testability

--- local/src/localSite.ts
+++ local/src/localSite.ts
@@ -1,105 +1,34 @@
-import { EventEmitter } from "events";
-
-import * as VError from "verror";
-
 import { LocalConfig } from "./config";
-import { ClientCode, ServerCode } from "./envisalink/codes";
 import { ClientMessage, ServerMessage } from "./envisalink/message";
-import { LocalSiteConnector } from "./localSiteConnector";
-import createLogger, { Logger } from "./logger";
-import { getLoginRes, LoginRes } from "./loginRes";
+import { LocalSiteConnectionManager } from "./localSiteConnectionManager";
+import { LocalSiteStateManager } from "./localSiteStateManager";
 
 export class LocalSite {
-  private readonly password: string;
-  private readonly connector: LocalSiteConnector;
-  private readonly emitter = new EventEmitter();
-  private readonly statusRefreshIntervalMs: number;
-  private readonly keepAliveIntervalMs: number;
-  private loggedIn: boolean = false;
-  private readonly logger: Logger;
+  private readonly connectionManager: LocalSiteConnectionManager;
+  private readonly stateManager: LocalSiteStateManager;
 
   constructor(config: LocalConfig) {
-    this.password = config.password;
-    this.emitter = new EventEmitter();
-    this.connector = new LocalSiteConnector(config.port, config.hostname);
-    this.statusRefreshIntervalMs = config.statusRefreshIntervalMs;
-    this.keepAliveIntervalMs = config.keepAliveIntervalMs;
-    this.logger = createLogger(__filename, {
-      tpiServer: config.hostname + ":" + config.port
-    });
+    this.connectionManager = new LocalSiteConnectionManager(
+      config.port,
+      config.hostname
+    );
+
+    this.stateManager = new LocalSiteStateManager(
+      config,
+      this.connectionManager
+    );
   }
 
   async start() {
-    this.connector.onMessage(msg => this.processMessage(msg));
-    this.connector.start();
-
-    setInterval(() => {
-      if (this.loggedIn) {
-        this.sendMessage(new ClientMessage(ClientCode.StatusReport));
-      }
-    }, this.statusRefreshIntervalMs);
-
-    setInterval(() => {
-      if (this.loggedIn) {
-        this.sendMessage(new ClientMessage(ClientCode.Poll));
-      }
-    }, this.keepAliveIntervalMs);
-  }
-
-  onMessage(listener: (msg: ServerMessage) => void) {
-    this.emitter.on("message", listener);
-  }
-
-  private processMessage(msg: ServerMessage) {
-    this.logger.debug("received", msg.toString());
-    switch (msg.code) {
-      case ServerCode.LoginRes:
-        this.processLoginRequest(msg);
-        break;
-      case ServerCode.Ack:
-        // do nothing
-        break;
-      default:
-        this.emitter.emit("message", msg);
-        break;
-    }
+    this.connectionManager.start();
+    this.stateManager.start();
   }
 
   async sendMessage(msg: ClientMessage) {
-    await this.connector.sendMessage(msg);
+    this.stateManager.sendMessage(msg);
   }
 
-  private processLoginRequest(msg: ServerMessage) {
-    const loginRes = getLoginRes(msg.data!.toString());
-    switch (loginRes) {
-      case LoginRes.Success:
-        this.loggedIn = true;
-        this.requestSiteRefresh();
-        break;
-      case LoginRes.Timeout:
-      case LoginRes.LoginRequest:
-        const loginMsg = new ClientMessage(
-          ClientCode.NetworkLogin,
-          Buffer.from(this.password)
-        );
-        this.sendMessage(loginMsg);
-        break;
-      case LoginRes.Failure:
-        throw new VError(
-          { name: "PasswordRejected" },
-          "Panic! Password rejected"
-        );
-      default:
-        throw new VError(
-          { name: "UnexpectedLogingResult", info: { message: msg.toString() } },
-          "Unexpected login result"
-        );
-    }
-  }
-
-  private requestSiteRefresh() {
-    if (this.loggedIn) {
-      this.sendMessage(new ClientMessage(ClientCode.StatusReport));
-    }
+  async onMessage(listener: (msg: ServerMessage) => void) {
+    this.stateManager.onMessage(listener);
   }
 }
---
+++ local/src/localSiteConnectionManager.ts
@@ -0,0 +1,112 @@
+import { EventEmitter } from "events";
+import { Socket } from "net";
+
+import * as VError from "verror";
+
+import createLogger from "./logger";
+
+const logger = createLogger(__filename);
+
+const MAX_ATTEMPTS = 1 << 10;
+const MAX_BACKOFF_EXP = 16;
+const MAX_WRITE_BUFFER_SIZE = 1 << 16;
+
+export class LocalSiteConnectionManager {
+  private readonly port: number;
+  private readonly hostname: string;
+  private readonly socket = new Socket();
+  private readonly emitter = new EventEmitter();
+  private connected = false;
+  private connAttempts = 0;
+  private buf = Buffer.alloc(0);
+  private writeBuffer = Buffer.alloc(0);
+
+  constructor(port: number, hostname: string) {
+    this.port = port;
+    this.hostname = hostname;
+  }
+
+  start() {
+    this.socket.on("data", (data: Buffer) => {
+      logger.debug("received:", data);
+      this.emitter.emit("data", data);
+    });
+
+    this.socket.on("close", () => {
+      logger.warn("socket was closed");
+      this.waitAndReconnect();
+    });
+
+    this.socket.on("error", err => {
+      logger.error("Socket error:", err);
+    });
+
+    this.socket.on("connect", () => {
+      logger.info(`Connected to ${this.hostname}:${this.port}`);
+      this.connAttempts = 0;
+      this.connected = true;
+      this.consumeWriteBuffer();
+    });
+
+    this.connect();
+  }
+
+  private connect() {
+    if (this.connAttempts >= MAX_ATTEMPTS) {
+      throw new VError(
+        {
+          name: "MaxConnectionAttemptsReached",
+          info: { attempts: this.connAttempts }
+        },
+        "Maximum connection attempts reach, givin up"
+      );
+    }
+    this.connAttempts++;
+    this.socket.connect(
+      this.port,
+      this.hostname
+    );
+  }
+
+  private waitAndReconnect() {
+    const backoff = 1 << Math.min(MAX_BACKOFF_EXP, this.connAttempts);
+    const delay = 500 + Math.random() * backoff;
+    logger.info(
+      `Disconnected, sleeping for ${delay} millis before attempting reconnect...`
+    );
+    setTimeout(() => this.connect(), delay);
+  }
+
+  onData(handler: (data: Buffer) => void) {
+    this.emitter.on("data", handler);
+  }
+
+  sendData(data: Buffer): void {
+    if (this.writeBuffer.length + data.length > MAX_WRITE_BUFFER_SIZE) {
+      throw new VError(
+        {
+          name: "MaxWriteBufferSizeExceeded",
+          info: { writeBuffer: this.writeBuffer.length, data: data.length }
+        },
+        "Maximum write buffer size exceeded"
+      );
+    }
+    this.writeBuffer = Buffer.concat([this.writeBuffer, data]);
+    this.consumeWriteBuffer();
+  }
+
+  private consumeWriteBuffer() {
+    if (!this.connected) {
+      return;
+    }
+
+    const data = this.writeBuffer;
+    this.writeBuffer = Buffer.alloc(0);
+
+    this.socket.write(data, (err?: Error) => {
+      if (err != null) {
+        logger.error(err, "error while trying to write to socket");
+      }
+    });
+  }
+}
